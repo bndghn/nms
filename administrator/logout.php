0cfe01cc9c03a71f0a9670b99072c25c339d53b0
bndghn <info@beny.ir>
2017-06-14 02:50:37 +0430

    add destory cookie process

--- administrator/logout.php
+++ administrator/logout.php
@@ -5,6 +5,9 @@ include('../core/config.php');
 (isset($_GET['error']) ? $error = intval($_GET['error']) : $error = "");
 (isset($_GET['message']) ? $message = intval($_GET['message']) : $message = "");
 
+// destory Cookie
+destroy_remember($_SESSION['USERNAME']);
+
 $_SESSION['ADMIN_ID']       = "";
 $_SESSION['ADMIN_ID']       = "";
 $_SESSION['ADMIN_USER']     = "";
@@ -17,7 +20,7 @@ $_SESSION['ADMIN_MOBILE']   = "";
 $_SESSION['ISADMIN']        = 1;
 
 session_destroy();
-if($error==="" AND $ $message==="" ){
+if($error === "" AND  $message === "" ){
     header("location: ".$config['adminurl']);
 }elseif($message!=""){
     header("location:$config[adminurl]/index.php?message=$message"); 
