c9028a4e91d9aab85dfd80c4f79a7d07ea348f4b
bndghn <info@beny.ir>
2017-06-21 16:44:02 +0430

    send error to users.php

--- administrator/users.add.php
+++ administrator/users.add.php
@@ -26,52 +26,87 @@ if ($submit === 1){
     (isset($_POST['username']) ? $username = $_POST['username'] : $username = "");
     (isset($_POST['email']) ? $email = $_POST['email'] : $email = "");
     (isset($_POST['mobile']) ? $mobile = $_POST['mobile'] : $mobile = "");
-    (isset($_POST['usergroup']) ? $usergroup =intval($_POST['usergroup']):$usergroup ="");
+    (isset($_POST['usergroup']) ? $usergroup =intval($_POST['usergroup']):$usergroup =0);
     
 
     if($username==="")
     {
-
-        $error = "حتما باید نام کاربری را وارد نمایید.";
-
-    }
-    
-    if(!verify_valid_email($email)){
-            if($mobile==="")
-            {
-
-                $error = "حتما باید شماره تلفن همراه را وارد نمایید.";
-
-            }
-
-    }
-        
-    if($mobile==="")
+        $error = "1";
+    }elseif(!verifyUserData("username",$username)){
+        $error = "2";
+    }elseif(!verify_user_unique("username",$username)){
+        $error = "3";
+    }elseif(strlen($username) < 4){
+        $error = "4";
+    }elseif(strlen($username) > 12){
+        $error = "5";
+    }elseif($usergroup === 0){
+        $error = "6";
+    }elseif($email ==="" AND $mobile === "")
     {
-
-        $error = "حتما باید شماره تلفن همراه را وارد نمایید.";
-
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    if($error===""){
-        $catname    = $conn->qStr($catname);
-        $descript   = $conn->qStr($descript);
+        $error = "7";
+    }else{
         
-       $query=" INSERT INTO `users`(`username`, `email`, `mobile`,`user_group`) VALUES ('.$username.','.$email.','.$mobile.','.$usergroup.') ";
+        //check if email enter
+        if ($email != ""){
+            
+            if(!verifyUserData("email",$email)){
+                $error = "8";
+            }elseif(!verify_user_unique("email",$email)){
+                $error = "9";
+            }
+        }
+        if($mobile != ""){
+            
+            if(strlen($mobile) != 11){
+                $error = "10";
+            }elseif(!verifyUserData("mobile",$mobile)){
+                $error = "شماره تلفن همراه به درستی وارد نشده است.";
+            }elseif(!verify_user_unique("mobile",$mobile)){
+                $error = "شماره تلفن همراه تکراری است و کاربر قبلا ثبت شده است.";
+            }
+        }
+    }
+    
+    $pass = generatePass( 6 , "number");
+  
+        
+   
+    
+    // if not seen error insert new user into bank
+    if($error===""){
+        
+        /* ToDoo
+        **********************************
+        1) isAdmin must added for admin and employer users for accessing to backend site.
+        **********************************
+        */
+        $username    = $conn->qStr($username);
+        
+        if($email !=""){
+            $email   = $conn->qStr($email);
+        }else{ $email   = "null";}
+        
+        if($mobile !=""){
+            $mobile   = $conn->qStr($mobile);
+        }else{ $mobile   = "null";}
+        
+        $firstPass  = $conn->qStr($pass);
+        $mdPass     = md5($pass);
+        
+        
+        
+       $query=" INSERT INTO `users`(`username`, `email`, `mobile`,`user_group`, `firstPass` , `pass`) VALUES ($username,$email,$mobile,$usergroup,$firstPass, '$mdPass') ";
+        echo $query;
        if($conn->EXECUTE($query)){
            header("location: ".$config['adminurl']."/users.php");
        } 
-        else{
-           $error=$conn->errorMsg(); 
-        }
+        
 
+    }else{
+        
+        header("location:".$config['adminurl']."/users.php?tsError=".$error);
+   
     }
 
 
