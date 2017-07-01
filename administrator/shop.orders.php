f250fcb6e283321dbb50f860748fb89dbf2cc2b1
erfanht <erfan.tiger021@gmail.com>
2017-07-01 11:45:35 +0430

    added just base codes to show pages just this

---
+++ administrator/shop.orders.php
@@ -0,0 +1,60 @@
+<?php
+
+include('../core/config.php');
+
+//check login or not
+verify_login_admin();
+    
+// define var
+$error      =   "";
+$message    =   "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//load nessary template for loading
+STemplate::assign('message',$message);
+STemplate::assign('error',$error);
+
+
+
+STemplate::assign('section',"shop");
+STemplate::assign('page',"shop_orders");
+
+
+STemplate::display('administrator/header.tpl');
+STemplate::display('administrator/shop.orders.tpl');
+STemplate::display('administrator/footer.tpl'); 
+?>
