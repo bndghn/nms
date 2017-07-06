a481097c7c64a6e9699531616f5d0ec3c603e0fe
erfanht <erfan.tiger021@gmail.com>
2017-07-06 10:40:19 +0430

    added event for delete product it means change pro status to 0

--- administrator/shop.catalog.delete.php
+++ administrator/shop.catalog.delete.php
@@ -9,37 +9,20 @@ verify_login_admin();
 $error = "";
 $message = "";
 
-
-  
 (isset($_GET['proid']) ? $proid =intval($_GET['proid']) : $proid = 0 );
 
-(isset($_POST['isSubmit']) ? $isSubmit = intval($_POST['isSubmit']) : $isSubmit = 0);
-
-if ($isSubmit === 1){
-  
-    $pro_status =intval($_POST['pro_status']);
-    $proid =intval($_POST['proid']);
-
+if ($proid != 0){
   if($error === "" ){
-    
         $query="UPDATE `shop_product` SET `pro_status`=0 WHERE `proid`=$proid";
              echo $query;  
        if($conn->EXECUTE($query)){
            header("location: ".$config['adminurl']."/shop.catalog.php");
        } 
         else{
-           $error=$conn->errorMsg(); 
+            echo $conn->errorMsg(); 
         }
-
     }else{
         header("location: ".$config['adminurl']."/shop.catalog.php?tsError=$error");
     }
 }
-
-      //load nessary template for loading
-    STemplate::assign('message',$message);
-    STemplate::assign('error',$error);
-
-STemplate::assign('section',"shop");
-STemplate::assign('page',"shop_catalog");
 ?>
--- administrator/shop.catalog.php
+++ administrator/shop.catalog.php
@@ -1,3 +1,4 @@
+
 <?php
 
 include('../core/config.php');
