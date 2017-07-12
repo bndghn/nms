bf3d9b144a8eb748c7951131468f959dc2190f54
bndghn <info@beny.ir>
2017-07-12 11:11:20 +0430

    added check login process

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
 include ('core/config.php');
+if($config['public'] === "0"){
+   verify_login();
+}
+
+(isset($_SESSION['USR_ID']) ? $usrID = $_SESSION['USR_ID'] : $usrID = "");
+(isset($_SESSION['USR_USER']) ? $usrUN = $_SESSION['USR_USER'] : $usrUN = "");
+(isset($_SESSION['USR_PASS']) ? $usrPSS = $_SESSION['USR_PASS'] : $usrPSS = "");
+
 STemplate::assign('test',"test");
-STemplate::display('test.tpl');
+STemplate::display('fs.tpl');
 ?>
