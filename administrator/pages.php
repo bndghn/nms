bdcb92df776cdc2fb8107de38acaf5540f286e97
bndghn <info@beny.ir>
2017-07-15 02:07:39 +0430

    added table list of pages

--- administrator/pages.php
+++ administrator/pages.php
@@ -11,53 +11,23 @@ $error      =   "";
 $message    =   "";
 
 
-
-(isset($_POST['isSubmit']) ? $submit = intval($_POST['isSubmit']) : $submit = 0);
-
-if ($submit === 1){
-    (isset($_POST['title']) ? $title = $_POST['title'] : $title = "");
-    (isset($_POST['slug']) ? $slug = $_POST['slug'] : $slug = "");
-    (isset($_POST['content']) ? $content = $_POST['content'] : $content = "");
-    (isset($_POST['status']) ? $status = 1 : $status = 0);
-    if($title === "" )
-    {
-        $error = "حتما باید عنوان را وارد نمایید.";
-    } 
-    elseif($content === "" )
-    {
-        $error = "حتما باید متن را وارد نمایید.";
-    } elseif($slug === "" )
-    {
-        $error = "حتما باید متن را وارد نمایید.";
-    } 
-
-
-    if($error === "" ){
-       $title = $conn->qStr($title);
-       $slug = $conn->qStr($slug);
-       $content = $conn->qStr($content);
-
-       $query="INSERT INTO `pages`(`title`, `slug`, `content`, `date`, `status`) VALUES ($title,$slug,$content,'".time()."',$status)";
-       if($conn->EXECUTE($query)){
-           header("location: ".$config['adminurl']."/pages.php");
-       } 
-       else{
-         $error=$conn->errorMsg(); 
-       }
-      
-    }
-
-
-}
 //load nessary template for loading
 STemplate::assign('message',$message);
 STemplate::assign('error',$error);
 
+(isset($_GET['delete']) ? $dltID = intval($_GET['delete']) : $dltID = 0);
+if($dltID !=0){
+	$query = "DELETE FROM `pages` WHERE `pgid`=$dltID";
+	if($conn->EXECUTE($query)){
+		$message = "صفحه با موفقیت حذف شد.";
+	} 
+	else{
+		$error=$conn->errorMsg(); 
+	}
+}
 
-
-
-STemplate::assign('section',"");
-STemplate::assign('page',"");
+STemplate::assign('section',"pages_manager");
+STemplate::assign('page',"pages");
 
 
 STemplate::display('administrator/pages.tpl');
